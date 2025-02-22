eee2e04052fd303ab407a449effe21aaf793ce30
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-02-22 13:59:16 +0200

    refactoring

--- src/decoder.py
+++ src/decoder.py
@@ -29,6 +29,30 @@ def convert_serializable(data):
     else:
         return str(data)
 
+def decode(decoded_lines):
+    # Read the input file decode it and save to a file
+    print("Decoding started...")
+    with open(input_file, 'r') as input:
+        reader = csv.reader(input, delimiter='\t')
+        for line in reader:
+            timestamp = line[0]
+            # TODO: is canID the right term? BO_ in .dbc
+            canID = parse_canID(line[1]) # TODO: error handling
+            data = line[2]
+            padded_data_bytes = bytes.fromhex(data.zfill(16)) # pad to 8-byte value
+            # decode the message from the database
+            try:
+                decoded_data = db.decode_message(canID, padded_data_bytes)
+                message = db.get_message_by_frame_id(canID)
+                # TODO: query should be optional This assumes it's mandatory
+                if message.name == query:
+                    decoded_line = generate_output(timestamp, message.name, decoded_data)
+                    decoded_lines.append(decoded_line)
+            except KeyError:
+                continue	# TODO: what do we do with the non found values?
+    print("Decoding ready.")
+
+
 # Check if the correct number of command line arguments is provided
 if len(sys.argv) != 4:
     print("Usage: python3 " + sys.argv[0] + " input_file vehicle_dbc_file query")
@@ -41,38 +65,12 @@ query = sys.argv[3]
 
 db = cantools.database.load_file(vehicle_db_file)
 decoded_lines = []
+decode(decoded_lines)
 
-# Read the input file decode it and save to a file
-print("Decoding started...")
-with open(input_file, 'r') as input:
-    reader = csv.reader(input, delimiter='\t')
-    for line in reader:
-        timestamp = line[0]
-        # TODO: is canID the right term? BO_ in .dbc
-        canID = parse_canID(line[1]) # TODO: error handling
-        data = line[2]
-        padded_data_bytes = bytes.fromhex(data.zfill(16)) # pad to 8-byte value
-        # decode the message from the database
-        try:
-            decoded_data = db.decode_message(canID, padded_data_bytes)
-            message = db.get_message_by_frame_id(canID)
-            # TODO: query should be optional This assumes it's mandatory
-            if message.name == query:
-                decoded_line = generate_output(timestamp, message.name, decoded_data)
-                decoded_lines.append(decoded_line)
-        except KeyError:
-            continue	# TODO: what do we do with the non found values?
-print("Decoding ready. Saving the results..")
-
+print("Saving the results")
 # Save the output to a file
 with open('decoder_output.txt', 'a') as outputfile:
     json.dump(decoded_lines, outputfile, indent=2)
 
 print("Decoder output file created")
-
-
-
-
-
-
 
