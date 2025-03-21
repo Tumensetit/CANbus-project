157647dbdef6aedf4080ed710964ad82c45ddf18
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-03-21 18:02:58 +0200

    separate main function sfrom others

--- src/decoder.py
+++ src/decoder.py
@@ -36,11 +36,12 @@ def convert_serializable(data):
     else:
         return str(data)
 
-def decode(decoded_lines):
+def decode(decoded_lines, vehicle_db_file, input_file, query):
     # Read the input file decode it and save to a file
     print("Decoding started...")
     with open(input_file, 'r') as input:
         reader = csv.reader(input, delimiter='\t')
+        db = cantools.database.load_file(vehicle_db_file)
         for line in reader:
             timestamp = line[0]
             # TODO: is canID the right term? BO_ in .dbc
@@ -80,39 +81,4 @@ def show_stats(decoded_lines):
         print(f"{key}: {stddev:.6f}")
         diffpriv_stats(key, values)
 
-
-## Main starts here
-parser = argparse.ArgumentParser(description="Argument parser for Decoder")
-
-parser.add_argument("-i", "--inputfile", type=str, help="Name of the file that contains the data to be decoded")
-parser.add_argument("-d", "--dbcfile", type=str, help="Name of the file that is used to decode the inputfile(ends in .dbc)")
-parser.add_argument("-q", "--query", type=str, help="Which ECUs data would you like to query? example given: BRAKE")
-
-args = parser.parse_args()
-
-# Check if the correct number of command line arguments is provided
-#if len(sys.argv) != 4:
-#    print("Usage: python3 " + sys.argv[0] + " input_file vehicle_dbc_file query")
-#    sys.exit(1)
-
-# Get file names from command line arguments
-input_file = args.inputfile
-vehicle_db_file = args.dbcfile
-query = args.query
-
-db = cantools.database.load_file(vehicle_db_file)
-decoded_lines = []
-decode(decoded_lines)
-
-if len(decoded_lines) == 0:
-    print("No lines found.")
-    sys.exit()
-
-print("Saving the results")
-# Save the output to a file
-with open('decoder_output.txt', 'a') as outputfile:
-    json.dump(decoded_lines, outputfile, indent=2)
-
-print("Decoder output file created")
-show_stats(decoded_lines)
 
