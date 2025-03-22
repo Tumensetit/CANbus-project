ffbddd0d73b7c6e9bbbf387f94c47828992bec80
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-03-22 11:44:39 +0200

    make diffpriv optional

--- src/main.py
+++ src/main.py
@@ -11,8 +11,10 @@ parser = argparse.ArgumentParser(description="Argument parser for Decoder")
 parser.add_argument("-i", "--inputfile", type=str, help="Name of the file that contains the data to be decoded")
 parser.add_argument("-d", "--dbcfile", type=str, help="Name of the file that is used to decode the inputfile(ends in .dbc)")
 parser.add_argument("-q", "--query", type=str, help="Which ECUs data would you like to query? example given: BRAKE", required=False)
+parser.add_argument("--diffpriv", action='store_true', help="Print experimental diffpriv mean", required=False)
 
 args = parser.parse_args()
+
 
 # Check if the correct number of command line arguments is provided
 #if len(sys.argv) != 4:
@@ -23,6 +25,8 @@ args = parser.parse_args()
 input_file = args.inputfile
 vehicle_db_file = args.dbcfile
 query = args.query
+diffpriv = args.diffpriv
+
 
 decoded_lines = []
 decode(decoded_lines, vehicle_db_file, input_file, query)
@@ -37,5 +41,5 @@ with open('decoder_output.txt', 'a') as outputfile:
     json.dump(decoded_lines, outputfile, indent=2)
 
 print("Decoder output file created")
-show_stats(decoded_lines)
+show_stats(decoded_lines, diffpriv)
 
