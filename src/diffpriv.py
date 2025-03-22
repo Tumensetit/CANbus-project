c3e0b94660e792743ec3fb74f575a3c25c9216e2
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-03-22 11:32:55 +0200

    handle errors when running diffpriv stats

--- src/diffpriv.py
+++ src/diffpriv.py
@@ -11,11 +11,17 @@ def diffpriv_stats(key, data):
     epsilon = 1.0
     # Estimate lower and upper bounds privately
     # TODO: Differential privacy ei toimi näin. Tämä on riski yksityisyydelle. Mietittävä, miten tämä oikeasti pitäisi toteutttaa
-    warnings.filterwarnings("ignore", category=PrivacyLeakWarning)
-    lower_bound = quantile(data, 0.05, epsilon=epsilon)
-    upper_bound = quantile(data, 0.95, epsilon=epsilon)
-    warnings.resetwarnings()
-    if lower_bound > upper_bound:
-        lower_bound, upper_bound = upper_bound, lower_bound
-    dp_mean = mean(data, epsilon=epsilon, bounds=(lower_bound, upper_bound))
-    print(f"Experimental: Differentially Private Mean: {key}, {dp_mean}")
+    try:
+        warnings.filterwarnings("ignore", category=PrivacyLeakWarning)
+        lower_bound = quantile(data, 0.05, epsilon=epsilon)
+        upper_bound = quantile(data, 0.95, epsilon=epsilon)
+        warnings.resetwarnings()
+        if lower_bound > upper_bound:
+            lower_bound, upper_bound = upper_bound, lower_bound
+
+        dp_mean = mean(data, epsilon=epsilon, bounds=(lower_bound, upper_bound))
+    except Exception as e:
+        print(f"{key}: Error occurred when calculating experimental DiffPriv mean: {e}")
+        return
+
+    print(f"{key}: Experimental: Differentially Private Mean: {dp_mean}")
