2fa476b9b9eb510de45ca1c64d609007d2d31046
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-04-05 15:22:54 +0300

    more probable VSS mappings

--- src/vss.py
+++ src/vss.py
@@ -1,9 +1,17 @@
+# Experimental DBC to VSS signal conversion.
+# Notes:
+## This mapping is DBC specific. MD5sum of the the expected file:
+## 95e68160afa70e21a888fa655578a73d  data/toyota_rav4_hybrid_2017_pt_generated.dbc
+
+## Many DBC messages can have the same signal name.
+## This only maps signals, so it might map the signal name to wrong message
+
 def convertDataToVss(input_signals):
     dbc_signal_to_vss_signals = {
         "YAW_RATE": "Vehicle.AngularVelocity.Yaw",
         "STEERING_TORQUE": "Vehicle.MotionManagement.Steering.SteeringWheel.Torque",
         "SPEED": "Vehicle.Speed",
-        "CRUISE_ACTIVE": "ADAS.ADAS.CruiseControl.IsActive",
+        "CRUISE_ACTIVE": "ADAS.ADAS.CruiseControl.IsEnabled",
         "ODOMETER": "Vehicle.TraveledDistance",
         "DOOR_OPEN_FL": "Cabin.Cabin.Door.Row1.DriverSide.IsOpen",
         "DOOR_OPEN_RL": "Cabin.Cabin.Door.Row2.DriverSide",
